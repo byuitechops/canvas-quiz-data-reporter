53b30ee845e149035a729a8842b10b9261f652be
Farr <sfarri@byui.edu>
2019-01-10 21:25:50 -0700

    Started canvasQuizReader

--- quizDataReducer.js
+++ quizDataReducer.js
@@ -1,7 +1,32 @@
 
-function quizDataReducer (data) {
+function quizDataReducer (courseData) {
     // SETH TODO Write this function to the best you can AFTER finishing TODOs in quizDataGatherer.js
-    return data;
+    reducedQuestions = courseData.map((course) => {
+        let allQuestions = {
+            questionBank: [],
+            canvasQuiz: []
+        };
+        //console.log(course.canvasQuiz);
+        //allQuestions.questionBank = questionBankReader(course.questionBank);
+        allQuestions.canvasQuiz = canvasQuizReader(course.canvasQuiz);
+        return allQuestions;
+    });
+    return reducedQuestions;
+}
+
+function questionBankReader(questionBank) {
+
+    return questions;
+}
+
+function canvasQuizReader(canvasQuiz) {
+    var questions = [];
+    //doesn't work, dunno why. M'tired.
+    canvasQuiz.foreach((quiz) => {
+        questions.push(quiz.byuiQuestions);
+    });
+    console.log(questions);
+    return questions;
 }
 
 module.exports = quizDataReducer;
